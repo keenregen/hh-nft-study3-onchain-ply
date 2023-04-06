ae86ce12793cd4f47bd65c594f59f5487826db5f
Keen Regen <keenregen@gmail.com>
2023-04-06 18:18:19 +0300

    v1.5

--- contracts/NftThree.sol
+++ contracts/NftThree.sol
@@ -15,4 +15,49 @@ contract NftThree is ERC721URIStorage {
     mapping(uint256 => uint256) public tokenIdToLevels;
 
     constructor() ERC721("NftThreeOnChain", "THREE") {}
+
+    function generateImg(uint256 tokenId) public returns (string memory) {
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+            "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
+            '<rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Img",
+            tokenId.toString(),
+            "</text>",
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Levels: ",
+            tokenIdToLevels[tokenId].toString(),
+            "</text>",
+            "</svg>"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(svg)
+                )
+            );
+    }
+
+    function getTokenURI(uint256 tokenId) public returns (string memory) {
+        bytes memory dataURI = abi.encodePacked(
+            "{",
+            '"name": "Image Comparisons #',
+            tokenId.toString(),
+            '",',
+            '"description": "Comparisons on chain",',
+            '"image": "',
+            generateImg(tokenId),
+            '"',
+            "}"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(dataURI)
+                )
+            );
+    }
 }
